9cea95893198f999aacde861dfc06ecdef9ac250
alicia65 <alicia65>
2020-02-04 14:19:01 -0600

    Add project files.

---
+++ Full time, Part time/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Full_time__Part_time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.trkCredits = new System.Windows.Forms.TrackBar();
+            this.lblCredits = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of credits";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(159, 187);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(196, 23);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status shown here";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trkCredits
+            // 
+            this.trkCredits.Location = new System.Drawing.Point(87, 128);
+            this.trkCredits.Maximum = 24;
+            this.trkCredits.Name = "trkCredits";
+            this.trkCredits.Size = new System.Drawing.Size(268, 56);
+            this.trkCredits.TabIndex = 4;
+            this.trkCredits.Scroll += new System.EventHandler(this.trkCredits_Scroll);
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.Location = new System.Drawing.Point(386, 138);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(0, 17);
+            this.lblCredits.TabIndex = 5;
+            this.lblCredits.Text = "0";
+            this.lblCredits.Click += new System.EventHandler(this.lblCredits_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 275);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.trkCredits);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Full time, half time_TrackBar?";
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TrackBar trkCredits;
+        private System.Windows.Forms.Label lblCredits;
+    }
+}
+
